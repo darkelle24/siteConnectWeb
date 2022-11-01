9ea30aa7ce1662488e0a0953a39a93f0f8c2ecd2
Djilani CARDINEAU <djilani.cardineau@epitech.eu>
2022-11-01 14:48:43 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,10 +18,10 @@ export class AppComponent {
     { name: 'lol', strenght: 10 },
     { name: 'lol', strenght: 10 },
     { name: 'lol', strenght: 10 },
+    { name: 'lol', strenght: 13 },
+    { name: 'lol', strenght: 12 },
     { name: 'lol', strenght: 10 },
-    { name: 'lol', strenght: 10 },
-    { name: 'lol', strenght: 10 },
-    { name: 'lol', strenght: 10 },
+    { name: 'lol', strenght: 15 },
     { name: 'lol', strenght: 10 },
     { name: 'lol', strenght: 10 },
     { name: 'lol', strenght: 10 },
@@ -34,13 +34,16 @@ export class AppComponent {
   }
 
   getWifi() {
-    this.isLoading = true
+    //this.isLoading = true
     this.wifi = undefined
     this.http.get(environment.url + '/wifis').subscribe({
       next: (value: any) => {
         console.log(value);
         this.wifiList = Object.keys(value).map((oneValue) => {
           return {name: oneValue, strenght: value[oneValue]}
+        })
+        this.wifiList.sort(function(a, b) {
+          return b.strenght - a.strenght;
         })
         console.log(this.wifi)
         this.isLoading = false;
@@ -54,7 +57,7 @@ export class AppComponent {
 
   tryToConnect() {
     this.isTryToConnect = true;
-    this.http.post(environment.url + '/connect', { ssid: this.wifi, password: this.password }).subscribe({
+    this.http.post(environment.url + '/connect', { ssid: this.wifi[0], password: this.password }).subscribe({
       next: (value: any) => {
         console.log(value);
         this.isTryToConnect = false;
